2afabb57f2be09d2afc02ffd03bab2dbd0cc7645
Tim <tijohask@gmail.com>
2025-01-23 13:58:28 -0500

    Creating a web scraper specifically for Rumble for now. Method returns a dict with the title, datetime and link. Can in theory add more in future.

--- __main__.py
+++ __main__.py
@@ -1,16 +1,25 @@
 import logging
 import os
+import argparse
 
 from scraper import get_next_vid_link
 from download import download_content
 from transcribe import transcribe_file
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--channel", help="The rumble channel to get the data out of")
+args = parser.parse_args()
+print(args.channel)
+channel = args.channel
+#channel = "https://rumble.com/c/AlexanderMercouris"
 
 def load_config(config_file: str):
     pass
 
 
 def main():
-    pass
+    for vid in get_next_vid_link(channel):
+        print(vid)
 
 
 
--- scraper.py
+++ scraper.py
@@ -1,4 +1,48 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+def make_rumble_request(channel, page_num, allow_redirects=True):
+    #print(page_num)
+#    channel = "https://rumble.com/c/AlexanderMercouris"
+    request_url = f"{channel}?page={page_num}"
+    return requests.get(
+        request_url,
+        headers={
+            "Host": "rumble.com",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:133.0) Gecko/20100101 Firefox/133.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-User": "?1"
+        }
+    )
 
 
-def get_next_vid_link():
-    pass
+def get_next_vid_link(channel):
+    i = 1
+    while True:
+        req = make_rumble_request(channel, i)
+        print(req.status_code)
+        if req.status_code > 299:
+            print(req.status_code)
+            break
+        content = req.content
+        soup = BeautifulSoup(content, 'html.parser')
+        all_divs = soup.find_all('div', class_="videostream__footer")
+        for div in all_divs:
+            datetime = div.address.div.span.time['datetime']
+            title = div.a.h3.string.strip()
+            link = channel + div.a['href']
+            json = {
+                'datetime' : datetime,
+                'title'    : title,
+                'link'     : link,
+            }
+            yield json
+
