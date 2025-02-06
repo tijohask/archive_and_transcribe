e728397c3d592e26c2077ea30cf2b023d83af56a
Tim <tijohask@gmail.com>
2025-02-06 00:23:13 -0500

    Adding a check for previously existing files, and storing the files in a channel folder.

--- download.py
+++ download.py
@@ -1,14 +1,23 @@
+import os
 import yt_dlp
 
 ydl_opts = {
     'write_info_json': True,
-    'format': 'mp4-360p',#'m4a/bestaudio/best',
+    'format': 'mp4-360p-0/mp4-360p',
+    #'download_archive': 'archive',
     'postprocessors': [
         { 'key': 'FFmpegExtractAudio', 'preferredcodec': 'm4a' },
         { 'key': 'FFmpegMetadata' },
     ]
 }
 
-def download_content(link):
-    ydl = yt_dlp.YoutubeDL(ydl_opts)
-    info = ydl.extract_info(link)
+def download_content(vid):
+    #print(ydl_opts)
+    # Make sure the audio file does not already exist. 
+    # Assume the audio file is complete and without issue.
+    if not os.path.exists(f"./output/{vid['channel_name']}/{vid['title']}.m4a"):
+        # Adding to the ydl_opts here because I want to store every video in 
+        # a folder named after the channel.
+        ydl_opts['outtmpl'] = f"./output/{vid['channel_name']}/%(title)s.%(ext)s"
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        info = ydl.extract_info(vid['link'])
