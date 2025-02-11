fac1fb4166db427ab001d47e1dd9a7c985ee1efe
Tim <tijohask@gmail.com>
2025-02-11 13:03:38 -0500

    Removing os file check from the download, will move it to main for now.

--- download.py
+++ download.py
@@ -1,4 +1,3 @@
-import os
 import yt_dlp
 
 ydl_opts = {
@@ -13,11 +12,8 @@ ydl_opts = {
 
 def download_content(vid):
     #print(ydl_opts)
-    # Make sure the audio file does not already exist. 
-    # Assume the audio file is complete and without issue.
-    if not os.path.exists(f"./output/{vid['channel_name']}/{vid['title']}.m4a"):
-        # Adding to the ydl_opts here because I want to store every video in 
-        # a folder named after the channel.
-        ydl_opts['outtmpl'] = f"./output/{vid['channel_name']}/%(title)s.%(ext)s"
-        ydl = yt_dlp.YoutubeDL(ydl_opts)
-        info = ydl.extract_info(vid['link'])
+    # Adding to the ydl_opts here because I want to store every video in
+    # a folder named after the channel.
+    ydl_opts['outtmpl'] = f"./output/{vid['channel_name']}/{vid['title']}.%(ext)s"
+    ydl = yt_dlp.YoutubeDL(ydl_opts)
+    info = ydl.extract_info(vid['link'])
